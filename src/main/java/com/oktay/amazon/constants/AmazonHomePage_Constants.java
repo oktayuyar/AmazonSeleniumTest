8e393315656ddea23ca81bd066c2de75bcaeffab
sahabt <sahabt@192.168.60.100>
2018-04-21 00:27:44 +0300

    Successful login test automated.

---
+++ src/main/java/com/oktay/amazon/constants/AmazonHomePage_Constants.java
@@ -0,0 +1,15 @@
+package com.oktay.amazon.constants;
+
+import org.openqa.selenium.By;
+
+/**
+ * @author oktay
+ *
+ */
+public interface AmazonHomePage_Constants extends General_Constants{
+	
+	public String homePageTitle = "Amazon.com: Online Shopping for Electronics, Apparel, Computers, Books, DVDs & more";
+    public By loginPage = By.id("nav-link-accountList");
+    public String loginPageTitle = "Amazon Sign In";
+
+}
---
+++ src/main/java/com/oktay/amazon/constants/AmazonLoginPage_Constants.java
@@ -0,0 +1,13 @@
+package com.oktay.amazon.constants;
+
+import org.openqa.selenium.By;
+
+/**
+ * @author oktay
+ *
+ */
+public interface AmazonLoginPage_Constants extends General_Constants{
+	
+	public By emailTextField = By.id("ap_email");
+	public By continueButton = By.id("continue");
+}
